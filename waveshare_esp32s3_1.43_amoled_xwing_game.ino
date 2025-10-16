77d95069a30fc4097a8c34f804d602603cdb7f37
thelastoutpostworkshop <thelastoutpostworkshop@gmail.com>
2025-10-16 19:14:38 -0400

    ACCEL_SCALE and GYRO_SCALE comments

--- waveshare_esp32s3_1.43_amoled_xwing_game.ino
+++ waveshare_esp32s3_1.43_amoled_xwing_game.ino
@@ -40,8 +40,12 @@
 #define ROUND_DURATION_MS (ROUND_DURATION_SECONDS * 1000U)
 
 // Game sensitivity adjustments (lower value = easier; higher value = harder)
-#define ACCEL_SCALE 1.5f     // Converts accelerometer tilt into lateral thrust; higher values make tilting drive faster movement
-#define GYRO_SCALE 0.05f     // Blends the gyro rotation into the controls; increase for stronger stick rotation response
+#define ACCEL_SCALE 1.5f     // Converts accelerometer tilt into vertical (up & down) thrust;
+                             // higher values make tilting drive faster X-Wing movement.
+
+#define GYRO_SCALE 0.05f  // Controls how sensitive horizontal movement is to board twisting;
+                          // increase to make the X-Wing move faster left and right.
+
 #define DAMPING 0.92f        // 1.0 = glide forever, 0.0 = stop instantly
 #define XWING_TARGET_AREA 30 // Lower = larger bullseye (easier), higher = tighter bullseye (harder)
 
